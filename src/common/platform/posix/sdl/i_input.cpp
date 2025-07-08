7824778b6bd7c7b9a84ab9fe6f0ba0010438ab19
Marcus Minhorst <marcus.minhorst@gmail.com>
2025-07-07 22:02:53 -0400

    Implemented sdl stubs and improved controller reconnection
    (cherry picked from commit d7bb21e2e30bacc329e1252f5f233e58a5423878)

--- src/common/platform/posix/sdl/i_input.cpp
+++ src/common/platform/posix/sdl/i_input.cpp
@@ -32,6 +32,7 @@
 */
 #include <SDL.h>
 #include <SDL_events.h>
+#include "i_input.h"
 #include "c_cvars.h"
 #include "dobject.h"
 #include "m_argv.h"
@@ -39,7 +40,6 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
-
 #include "d_eventbase.h"
 #include "d_gui.h"
 #include "c_buttons.h"
@@ -52,9 +52,6 @@
 #include "engineerrors.h"
 #include "i_interface.h"
 #include "m_joy.h"
-
-static void I_CheckGUICapture ();
-static void I_CheckNativeMouse ();
 
 bool GUICapture;
 static bool NativeMouse = true;
@@ -471,12 +468,12 @@ void MessagePump (const SDL_Event &sev)
 
 	case SDL_JOYBUTTONDOWN:
 	case SDL_JOYBUTTONUP:
-		if (SDL_IsGameController(sev.jdevice.which))
+		if (SDL_GameControllerFromInstanceID(sev.jdevice.which))
 			break; // let SDL_CONTROLLERBUTTON* handle this
 		event.type = sev.type == SDL_JOYBUTTONDOWN ? EV_KeyDown : EV_KeyUp;
 		event.data1 = KEY_FIRSTJOYBUTTON + sev.jbutton.button;
 		if(event.data1 != 0)
-			D_PostEvent(&event);
+			I_JoyConsumeEvent(sev.jdevice.which, &event);
 		break;
 	case SDL_CONTROLLERBUTTONDOWN:
 	case SDL_CONTROLLERBUTTONUP:
@@ -507,14 +504,22 @@ void MessagePump (const SDL_Event &sev)
 			default:                                  event.data1 = 0;
 		}
 		if(event.data1 != 0)
-			D_PostEvent(&event);
+			I_JoyConsumeEvent(sev.cbutton.which, &event);
 		break;
 
 	case SDL_JOYDEVICEADDED:
-	case SDL_JOYDEVICEREMOVED:
 	case SDL_CONTROLLERDEVICEADDED:
+		if (sev.type == SDL_JOYDEVICEADDED && SDL_IsGameController(sev.jdevice.which)) // DeviceIndex Here
+			break; // skip double event
+		I_UpdateDeviceList();
+		event.type = EV_DeviceChange;
+		D_PostEvent (&event);
+		break;
+
+	case SDL_JOYDEVICEREMOVED:
 	case SDL_CONTROLLERDEVICEREMOVED:
-		if ((sev.type == SDL_JOYDEVICEADDED || sev.type == SDL_JOYDEVICEREMOVED) && SDL_IsGameController(sev.jdevice.which))
+	case SDL_CONTROLLERDEVICEREMAPPED:
+		if (sev.type == SDL_JOYDEVICEREMOVED && SDL_GameControllerFromInstanceID(sev.jdevice.which))
 			break; // skip double event
 		I_UpdateDeviceList();
 		event.type = EV_DeviceChange;
