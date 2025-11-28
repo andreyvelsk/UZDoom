emileb/gzdoom

Suqash mobile changes

src/common/platform/posix/sdl/i_input.cpp
@@ -245,6 +245,10 @@ void I_SetMouseCapture()
 	// Clear out any mouse movement.
 	SDL_GetRelativeMouseState (NULL, NULL);
 	SDL_SetRelativeMouseMode (SDL_TRUE);
+#ifdef __MOBILE__
+	// Need to clear this again because setting mode above adds relative values
+	SDL_GetRelativeMouseState (NULL, NULL);
+#endif
 }
 
 void I_ReleaseMouseCapture()

andreyvelsk/UZDoom

(cherry picked from commit 3e29c93e975c638589299f8b9e59ab8aa8ae1307)

src/common/platform/posix/sdl/i_input.cpp
@@ -255,6 +255,10 @@ void I_SetMouseCapture()
 	// Clear out any mouse movement.
 	SDL_GetRelativeMouseState (NULL, NULL);
 	SDL_CaptureMouse (SDL_TRUE);
+#ifdef ANDROID
+	// Need to clear this again because setting mode above adds relative values
+	SDL_GetRelativeMouseState (NULL, NULL);
+#endif	
 }
 
 void I_ReleaseMouseCapture()