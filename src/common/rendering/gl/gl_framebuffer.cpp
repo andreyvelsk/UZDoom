emileb/gzdoom

Squash mobile changes

src/common/rendering/gl/gl_framebuffer.cpp
@@ -175,8 +175,17 @@ void OpenGLFrameBuffer::InitializeState()
 	mBones = new BoneBuffer(screen->mPipelineNbr);
 	GLRenderer = new FGLRenderer(this);
 	GLRenderer->Initialize(GetWidth(), GetHeight());
+
 	static_cast<GLDataBuffer*>(mLights->GetBuffer())->BindBase();
 	static_cast<GLDataBuffer*>(mBones->GetBuffer())->BindBase();
+
+#ifdef __ANDROID__ 	// This is needed to stop Ardeno 530 from crashing on the first drawer
+	static_cast<GLDataBuffer*>(mLights->GetBuffer())->Map();
+	static_cast<GLDataBuffer*>(mLights->GetBuffer())->Unmap();
+
+	static_cast<GLDataBuffer*>(mBones->GetBuffer())->Map();
+	static_cast<GLDataBuffer*>(mBones->GetBuffer())->Unmap();
+#endif
 
 	mDebug = std::make_unique<FGLDebug>();
 	mDebug->Update();
@@ -200,7 +209,18 @@ void OpenGLFrameBuffer::Update()
 	Swap();
 	Super::Update();
 }
+#ifdef __MOBILE__
+uint8_t * gles_convertRGB(uint8_t* src, uint8_t * dst, int width, int height)
+{
+	for (int i=0; i<width*height; i++) {
+		for (int j=0; j<3; j++)
+			*(dst++) = *(src++);
+		src++;
+	}
 
+	return dst;
+}
+#endif
 void OpenGLFrameBuffer::CopyScreenToBuffer(int width, int height, uint8_t* scr)
 {
 	IntRect bounds;
@@ -212,7 +232,14 @@ void OpenGLFrameBuffer::CopyScreenToBuffer(int width, int height, uint8_t* scr)
 
 	// strictly speaking not needed as the glReadPixels should block until the scene is rendered, but this is to safeguard against shitty drivers
 	glFinish();
+#ifdef __MOBILE__
+	uint8_t* tmp = (uint8_t *)M_Malloc(width * height * 4);
+	glReadPixels(0, 0, width, height, GL_RGBA,GL_UNSIGNED_BYTE, tmp);
+	gles_convertRGB( tmp, scr, width, height);
+	M_Free(tmp);
+#else
 	glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, scr);
+#endif
 }
 
 //===========================================================================
@@ -281,6 +308,12 @@ void OpenGLFrameBuffer::Swap()
 
 		RenderState()->SetVertexBuffer(screen->mVertexData); // Needed for Raze because it does not reset it
 	}
+
+#ifdef __MOBILE__
+	GLRenderer->mShaderManager->SetActiveShader(0);
+#endif
+
+
 	Finish.Unclock();
 	camtexcount = 0;
 	FHardwareTexture::UnbindAll();

src/common/rendering/gl/gl_renderbuffers.cpp
@@ -304,18 +304,45 @@ PPGLTexture FGLRenderBuffers::Create2DTexture(const char *name, GLuint format, i
 	case GL_R16F:				dataformat = GL_RED; datatype = GL_FLOAT; break;
 	case GL_RG32F:				dataformat = GL_RG; datatype = GL_FLOAT; break;
 	case GL_RG16F:				dataformat = GL_RG; datatype = GL_FLOAT; break;
-	case GL_RGB10_A2:			dataformat = GL_RGBA; datatype = GL_UNSIGNED_INT_10_10_10_2; break;
+	case GL_RGB10_A2:			dataformat = GL_RGBA; datatype = GL_UNSIGNED_INT_2_10_10_10_REV; break;
 	case GL_DEPTH_COMPONENT24:	dataformat = GL_DEPTH_COMPONENT; datatype = GL_FLOAT; break;
 	case GL_STENCIL_INDEX8:		dataformat = GL_STENCIL_INDEX; datatype = GL_INT; break;
 	case GL_DEPTH24_STENCIL8:	dataformat = GL_DEPTH_STENCIL; datatype = GL_UNSIGNED_INT_24_8; break;
 	default: I_FatalError("Unknown format passed to FGLRenderBuffers.Create2DTexture");
 	}
 
+#ifdef __MOBILE__ // FIXME
+	if (format == GL_RGBA16F)
+	{
+		format = GL_RGBA;
+		dataformat = GL_RGBA;
+		datatype = GL_UNSIGNED_BYTE;
+	}
+
+	// Mobile does not have GL_RGBA16_SNORM so convert to GL_RGBA8_SNORM
+	// Used for SSAO random texture
+    if (format == GL_RGBA16_SNORM)
+	{
+		format = GL_RGBA8_SNORM;
+		dataformat = GL_RGBA;
+		datatype = GL_BYTE;
+		for(int n = 0; n < width * height; n++)
+		{
+			((int8_t*)data)[n] = ((int16_t*)data)[n] / 256;
+		}
+	}
+#endif
+
 	glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, dataformat, datatype, data);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+#ifdef __MOBILE__ // This is to fix AO on some devices (P20)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+#endif
 	return tex;
 }
 
@@ -480,7 +507,13 @@ void FGLRenderBuffers::ClearFrameBuffer(bool stencil, bool depth)
 	GLdouble depthValue;
 	glGetBooleanv(GL_SCISSOR_TEST, &scissorEnabled);
 	glGetIntegerv(GL_STENCIL_CLEAR_VALUE, &stencilValue);
+#ifdef __MOBILE__
+	GLfloat t;
+	glGetFloatv(GL_DEPTH_CLEAR_VALUE, &t);
+	depthValue = t;
+#else
 	glGetDoublev(GL_DEPTH_CLEAR_VALUE, &depthValue);
+#endif
 	glDisable(GL_SCISSOR_TEST);
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClearDepth(0.0);

src/common/rendering/gles/gles_framebuffer.cpp
@@ -180,7 +180,18 @@ void OpenGLFrameBuffer::Update()
 	Swap();
 	Super::Update();
 }
+#ifdef __MOBILE__
+uint8_t * gles_convertRGB(uint8_t* src, uint8_t * dst, int width, int height)
+{
+	for (int i=0; i<width*height; i++) {
+		for (int j=0; j<3; j++)
+			*(dst++) = *(src++);
+		src++;
+	}
 
+	return dst;
+}
+#endif
 void OpenGLFrameBuffer::CopyScreenToBuffer(int width, int height, uint8_t* scr)
 {
 	IntRect bounds;
@@ -192,7 +203,14 @@ void OpenGLFrameBuffer::CopyScreenToBuffer(int width, int height, uint8_t* scr)
 
 	// strictly speaking not needed as the glReadPixels should block until the scene is rendered, but this is to safeguard against shitty drivers
 	glFinish();
+#ifdef __MOBILE__
+	uint8_t* tmp = (uint8_t *)M_Malloc(width * height * 4);
+	glReadPixels(0, 0, width, height, GL_RGBA,GL_UNSIGNED_BYTE, tmp);
+	gles_convertRGB( tmp, scr, width, height);
+	M_Free(tmp);
+#else
 	glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, scr);
+#endif
 }
 
 //===========================================================================
@@ -251,6 +269,10 @@ void OpenGLFrameBuffer::Swap()
 	mVertexData->WaitSync();
 
 	RenderState()->SetVertexBuffer(screen->mVertexData); // Needed for Raze because it does not reset it
+
+#ifdef __MOBILE__
+	GLRenderer->mShaderManager->SetActiveShader(0);
+#endif
 
 	Finish.Unclock();
 	camtexcount = 0;

src/common/rendering/hwrenderer/data/hw_viewpointbuffer.cpp
@@ -122,6 +122,10 @@ void HWViewpointBuffer::Clear()
 	bool needNewPipeline = mUploadIndex > 0; // Clear might be called multiple times before any actual rendering
 
 	mUploadIndex = 0;
+#ifdef __MOBILE__ // Needed to fix screen wipe when it's first run on a launch. Otherwise viewport does not get set properly for the first frame
+	mLastMappedIndex = UINT_MAX;
+#endif
+
 	mClipPlaneInfo.Clear();
 
 	if (needNewPipeline)

src/common/rendering/hwrenderer/data/hw_vrmodes.cpp
@@ -45,7 +45,12 @@
 CVAR(Int, vr_mode, 0, CVAR_GLOBALCONFIG|CVAR_ARCHIVE)
 
 // switch left and right eye views
+#ifdef __MOBILE__
+CVAR(Bool, vr_swap_eyes, true, CVAR_GLOBALCONFIG | CVAR_ARCHIVE)
+#else
 CVAR(Bool, vr_swap_eyes, false, CVAR_GLOBALCONFIG | CVAR_ARCHIVE)
+#endif
+
 
 // intraocular distance in meters
 CVAR(Float, vr_ipd, 0.062f, CVAR_ARCHIVE|CVAR_GLOBALCONFIG) // METERS

andreyvelsk/UZDoom

src/common/rendering/gl/gl_framebuffer.cpp
@@ -175,8 +175,17 @@ void OpenGLFrameBuffer::InitializeState()
 	mBones = new BoneBuffer(screen->mPipelineNbr);
 	GLRenderer = new FGLRenderer(this);
 	GLRenderer->Initialize(GetWidth(), GetHeight());
+
 	static_cast<GLDataBuffer*>(mLights->GetBuffer())->BindBase();
 	static_cast<GLDataBuffer*>(mBones->GetBuffer())->BindBase();
+
+#ifdef __ANDROID__ 	// This is needed to stop Ardeno 530 from crashing on the first drawer
+	static_cast<GLDataBuffer*>(mLights->GetBuffer())->Map();
+	static_cast<GLDataBuffer*>(mLights->GetBuffer())->Unmap();
+
+	static_cast<GLDataBuffer*>(mBones->GetBuffer())->Map();
+	static_cast<GLDataBuffer*>(mBones->GetBuffer())->Unmap();
+#endif
 
 	mDebug = std::make_unique<FGLDebug>();
 	mDebug->Update();
@@ -200,7 +209,18 @@ void OpenGLFrameBuffer::Update()
 	Swap();
 	Super::Update();
 }
+#ifdef ANDROID
+uint8_t * gles_convertRGB(uint8_t* src, uint8_t * dst, int width, int height)
+{
+	for (int i=0; i<width*height; i++) {
+		for (int j=0; j<3; j++)
+			*(dst++) = *(src++);
+		src++;
+	}
 
+	return dst;
+}
+#endif
 void OpenGLFrameBuffer::CopyScreenToBuffer(int width, int height, uint8_t* scr)
 {
 	IntRect bounds;
@@ -212,7 +232,14 @@ void OpenGLFrameBuffer::CopyScreenToBuffer(int width, int height, uint8_t* scr)
 
 	// strictly speaking not needed as the glReadPixels should block until the scene is rendered, but this is to safeguard against shitty drivers
 	glFinish();
+#ifdef ANDROID
+	uint8_t* tmp = (uint8_t *)M_Malloc(width * height * 4);
+	glReadPixels(0, 0, width, height, GL_RGBA,GL_UNSIGNED_BYTE, tmp);
+	gles_convertRGB( tmp, scr, width, height);
+	M_Free(tmp);
+#else
 	glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, scr);
+#endif
 }
 
 //===========================================================================
@@ -281,6 +308,12 @@ void OpenGLFrameBuffer::Swap()
 
 		RenderState()->SetVertexBuffer(screen->mVertexData); // Needed for Raze because it does not reset it
 	}
+
+#ifdef ANDROID
+	GLRenderer->mShaderManager->SetActiveShader(0);
+#endif
+
+
 	Finish.Unclock();
 	camtexcount = 0;
 	FHardwareTexture::UnbindAll();

src/common/rendering/gl/gl_renderbuffers.cpp
@@ -304,18 +304,45 @@ PPGLTexture FGLRenderBuffers::Create2DTexture(const char *name, GLuint format, i
 	case GL_R16F:				dataformat = GL_RED; datatype = GL_FLOAT; break;
 	case GL_RG32F:				dataformat = GL_RG; datatype = GL_FLOAT; break;
 	case GL_RG16F:				dataformat = GL_RG; datatype = GL_FLOAT; break;
-	case GL_RGB10_A2:			dataformat = GL_RGBA; datatype = GL_UNSIGNED_INT_10_10_10_2; break;
+	case GL_RGB10_A2:			dataformat = GL_RGBA; datatype = GL_UNSIGNED_INT_2_10_10_10_REV; break;
 	case GL_DEPTH_COMPONENT24:	dataformat = GL_DEPTH_COMPONENT; datatype = GL_FLOAT; break;
 	case GL_STENCIL_INDEX8:		dataformat = GL_STENCIL_INDEX; datatype = GL_INT; break;
 	case GL_DEPTH24_STENCIL8:	dataformat = GL_DEPTH_STENCIL; datatype = GL_UNSIGNED_INT_24_8; break;
 	default: I_FatalError("Unknown format passed to FGLRenderBuffers.Create2DTexture");
 	}
 
+#ifdef ANDROID // FIXME
+	if (format == GL_RGBA16F)
+	{
+		format = GL_RGBA;
+		dataformat = GL_RGBA;
+		datatype = GL_UNSIGNED_BYTE;
+	}
+
+	// Mobile does not have GL_RGBA16_SNORM so convert to GL_RGBA8_SNORM
+	// Used for SSAO random texture
+    if (format == GL_RGBA16_SNORM)
+	{
+		format = GL_RGBA8_SNORM;
+		dataformat = GL_RGBA;
+		datatype = GL_BYTE;
+		for(int n = 0; n < width * height; n++)
+		{
+			((int8_t*)data)[n] = ((int16_t*)data)[n] / 256;
+		}
+	}
+#endif
+
 	glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, dataformat, datatype, data);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+#ifdef ANDROID // This is to fix AO on some devices (P20)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+#endif
 	return tex;
 }
 
@@ -480,7 +507,13 @@ void FGLRenderBuffers::ClearFrameBuffer(bool stencil, bool depth)
 	GLdouble depthValue;
 	glGetBooleanv(GL_SCISSOR_TEST, &scissorEnabled);
 	glGetIntegerv(GL_STENCIL_CLEAR_VALUE, &stencilValue);
+#ifdef ANDROID
+	GLfloat t;
+	glGetFloatv(GL_DEPTH_CLEAR_VALUE, &t);
+	depthValue = t;
+#else
 	glGetDoublev(GL_DEPTH_CLEAR_VALUE, &depthValue);
+#endif
 	glDisable(GL_SCISSOR_TEST);
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClearDepth(0.0);

src/common/rendering/gles/gles_framebuffer.cpp
@@ -180,7 +180,18 @@ void OpenGLFrameBuffer::Update()
 	Swap();
 	Super::Update();
 }
+#ifdef ANDROID
+uint8_t * gles_convertRGB(uint8_t* src, uint8_t * dst, int width, int height)
+{
+	for (int i=0; i<width*height; i++) {
+		for (int j=0; j<3; j++)
+			*(dst++) = *(src++);
+		src++;
+	}
 
+	return dst;
+}
+#endif
 void OpenGLFrameBuffer::CopyScreenToBuffer(int width, int height, uint8_t* scr)
 {
 	IntRect bounds;
@@ -192,7 +203,14 @@ void OpenGLFrameBuffer::CopyScreenToBuffer(int width, int height, uint8_t* scr)
 
 	// strictly speaking not needed as the glReadPixels should block until the scene is rendered, but this is to safeguard against shitty drivers
 	glFinish();
+#ifdef ANDROID
+	uint8_t* tmp = (uint8_t *)M_Malloc(width * height * 4);
+	glReadPixels(0, 0, width, height, GL_RGBA,GL_UNSIGNED_BYTE, tmp);
+	gles_convertRGB( tmp, scr, width, height);
+	M_Free(tmp);
+#else
 	glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, scr);
+#endif
 }
 
 //===========================================================================
@@ -251,6 +269,10 @@ void OpenGLFrameBuffer::Swap()
 	mVertexData->WaitSync();
 
 	RenderState()->SetVertexBuffer(screen->mVertexData); // Needed for Raze because it does not reset it
+
+#ifdef ANDROID
+	GLRenderer->mShaderManager->SetActiveShader(0);
+#endif
 
 	Finish.Unclock();
 	camtexcount = 0;

src/common/rendering/hwrenderer/data/hw_viewpointbuffer.cpp
@@ -122,9 +122,10 @@ void HWViewpointBuffer::Clear()
 	bool needNewPipeline = mUploadIndex > 0; // Clear might be called multiple times before any actual rendering
 
 	mUploadIndex = 0;
-#ifdef ANDROID
-    mLastMappedIndex = UINT_MAX;
+#ifdef ANDROID // Needed to fix screen wipe when it's first run on a launch. Otherwise viewport does not get set properly for the first frame
+	mLastMappedIndex = UINT_MAX;
 #endif
+
 	mClipPlaneInfo.Clear();
 
 	if (needNewPipeline)

src/common/rendering/hwrenderer/data/hw_vrmodes.cpp
@@ -45,7 +45,12 @@
 CVAR(Int, vr_mode, 0, CVAR_GLOBALCONFIG|CVAR_ARCHIVE)
 
 // switch left and right eye views
+#ifdef ANDROID
+CVAR(Bool, vr_swap_eyes, true, CVAR_GLOBALCONFIG | CVAR_ARCHIVE)
+#else
 CVAR(Bool, vr_swap_eyes, false, CVAR_GLOBALCONFIG | CVAR_ARCHIVE)
+#endif
+
 
 // intraocular distance in meters
 CVAR(Float, vr_ipd, 0.062f, CVAR_ARCHIVE|CVAR_GLOBALCONFIG) // METERS