glKarin/com.n0n3m4.diii4a

GZDOOM: add OpenGL renderer backend, add OpenGLES version config

Q3E/src/main/jni/gzdoom/src/common/rendering/gl/gl_framebuffer.cpp
@@ -212,7 +212,22 @@ void OpenGLFrameBuffer::CopyScreenToBuffer(int width, int height, uint8_t* scr)
 
 	// strictly speaking not needed as the glReadPixels should block until the scene is rendered, but this is to safeguard against shitty drivers
 	glFinish();
+#ifdef _GLES //karin: glReadPixels using GL_RGBA on OpenGLES
+	uint8_t *scr4 = (uint8_t *)malloc(4 * width * height);
+	glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, scr4);
+	for(int h = 0; h < height; h++)
+	{
+		for(int w = 0; w < width; w++)
+		{
+			const uint8_t *src = scr4 + (width * h + w) * 4;
+			uint8_t *dst = scr + (width * h + w) * 3;
+			memcpy(dst, src, 3);
+		}
+	}
+	free(scr4);
+#else
 	glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, scr);
+#endif
 }
 
 //===========================================================================
@@ -507,7 +522,22 @@ TArray<uint8_t> OpenGLFrameBuffer::GetScreenshotBuffer(int &pitch, ESSType &colo
 	TArray<uint8_t> pixels;
 	pixels.Resize(viewport.width * viewport.height * 3);
 	glPixelStorei(GL_PACK_ALIGNMENT, 1);
+#ifdef _GLES //karin: glReadPixels using GL_RGBA on OpenGLES
+	uint8_t *pixels4 = (uint8_t *)malloc(4 * viewport.width * viewport.height);
+	glReadPixels(viewport.left, viewport.top, viewport.width, viewport.height, GL_RGBA, GL_UNSIGNED_BYTE, pixels4);
+	for(int h = 0; h < viewport.height; h++)
+	{
+		for(int w = 0; w < viewport.width; w++)
+		{
+			const uint8_t *src = pixels4 + (viewport.width * h + w) * 4;
+			uint8_t *dst = (&pixels[0]) + (viewport.width * h + w) * 3;
+			memcpy(dst, src, 3);
+		}
+	}
+	free(pixels4);
+#else
 	glReadPixels(viewport.left, viewport.top, viewport.width, viewport.height, GL_RGB, GL_UNSIGNED_BYTE, &pixels[0]);
+#endif
 	glPixelStorei(GL_PACK_ALIGNMENT, 4);
 
 	// Copy to screenshot buffer:

Q3E/src/main/jni/gzdoom/src/common/rendering/gl/gl_hwtexture.cpp
@@ -139,20 +139,35 @@ unsigned int FHardwareTexture::CreateTexture(unsigned char * buffer, int w, int 
 	{
 		if (glTextureBytes < 4) glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 		static const int ITypes[] = { GL_R8, GL_RG8, GL_RGB8, GL_RGBA8 };
+#ifdef _GLES //karin: internal format must same as data format on OpenGLES
+		static const int STypes[] = { GL_RED, GL_RG, GL_RGB, GL_RGBA };
+#else
 		static const int STypes[] = { GL_RED, GL_RG, GL_BGR, GL_BGRA };
+#endif
 
 		texformat = ITypes[glTextureBytes - 1];
 		sourcetype = STypes[glTextureBytes - 1];
 	}
 	else
 	{
+#ifdef _GLES //karin: internal format must same as data format on OpenGLES
+		sourcetype = GL_RGBA;
+#else
 		sourcetype = GL_BGRA;
+#endif
 	}
 
 	if (!firstCall && glBufferID > 0)
 		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, rw, rh, sourcetype, GL_UNSIGNED_BYTE, buffer);
 	else
 		glTexImage2D(GL_TEXTURE_2D, 0, texformat, rw, rh, 0, sourcetype, GL_UNSIGNED_BYTE, buffer);
+#ifdef _GLES //karin: swap R and B on OpenGLES
+	if(sourcetype == GL_RGB || sourcetype == GL_RGBA)
+	{
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_R, GL_BLUE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, GL_RED);
+	}
+#endif
 
 	if (deletebuffer && buffer) free(buffer);
 	else if (glBufferID)

Q3E/src/main/jni/gzdoom/src/common/rendering/gl/gl_renderstate.cpp
@@ -424,7 +424,19 @@ void FGLRenderState::Draw(int dt, int index, int count, bool apply)
 		Apply();
 	}
 	drawcalls.Clock();
+#ifdef _GLES //karin: TODO glDrawArrays crash!!!
+	int buf;
+	glGetIntegerv( GL_ELEMENT_ARRAY_BUFFER_BINDING, &buf);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	int *arr = (int*)malloc(4 * count);
+	for(int i = 0; i < count; i++)
+		arr[i] = index + i;
+	TID(glDrawElements(dt2gl[dt], count, GL_UNSIGNED_INT, arr));
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buf);
+	free(arr);
+#else
 	glDrawArrays(dt2gl[dt], index, count);
+#endif
 	drawcalls.Unclock();
 }
 

Q3E/src/main/jni/gzdoom/src/common/rendering/gl/gl_shaderprogram.cpp
@@ -32,6 +32,22 @@
 
 namespace OpenGLRenderer
 {
+#ifdef _GLES //karin: control GLSL default precision
+CVAR(Int, gl_glsl_precision, 0, 0); // 0 = default(high), 1 = medium, 2 = low
+
+FString GetGLSLPrecision()
+{
+	FString str = "precision highp int;\n \
+			   precision highp float;\n";
+
+	if (gl_glsl_precision == 2)
+		str.Substitute("highp", "lowp");
+	else if (gl_glsl_precision == 1)
+		str.Substitute("highp", "mediump");
+
+	return str;
+}
+#endif
 
 bool IsShaderCacheActive();
 TArray<uint8_t> LoadCachedProgramBinary(const FString &vertex, const FString &fragment, uint32_t &binaryFormat);
@@ -267,7 +283,11 @@ FString FShaderProgram::PatchShader(ShaderType type, const FString &code, const 
 	// If we have 4.2, always use it because it adds important new syntax.
 	if (maxGlslVersion < 420 && gl.glslversion >= 4.2f) maxGlslVersion = 420;
 	int shaderVersion = min((int)round(gl.glslversion * 10) * 10, maxGlslVersion);
+#ifdef _GLES //karin: using #version 320 es on OpenGLES
+	patchedCode.AppendFormat("#version 300 es\n");
+#else
 	patchedCode.AppendFormat("#version %d\n", shaderVersion);
+#endif
 
 	// TODO: Find some way to add extension requirements to the patching
 	//
@@ -278,8 +298,12 @@ FString FShaderProgram::PatchShader(ShaderType type, const FString &code, const 
 		patchedCode << defines;
 
 	// these settings are actually pointless but there seem to be some old ATI drivers that fail to compile the shader without setting the precision here.
+#ifdef _GLES //karin: control GLSL default precision
+	patchedCode << GetGLSLPrecision();
+#else
 	patchedCode << "precision highp int;\n";
 	patchedCode << "precision highp float;\n";
+#endif
 
 	patchedCode << "#line 1\n";
 	patchedCode << RemoveLayoutLocationDecl(code, type == Vertex ? "out" : "in");

Q3E/src/main/jni/gzdoom/src/common/rendering/gles/gles_hwtexture.cpp
@@ -155,7 +155,11 @@ unsigned int FHardwareTexture::CreateTexture(unsigned char * buffer, int w, int 
 		else
 		{
 			sourcetype = GL_BGRA;
+#ifdef _GLES //karin: These two must be the same on OpenGLES
+			texformat = GL_BGRA;
+#else
 			texformat = GL_RGBA;
+#endif
 		}
 	}
 
@@ -168,8 +172,22 @@ unsigned int FHardwareTexture::CreateTexture(unsigned char * buffer, int w, int 
 		if (glTextureBytes == 1)
 		{
 			GLint swizzleMask[] = { GL_ZERO, GL_ZERO, GL_ZERO, GL_RED };
+#ifdef _GLES //karin: no GL_TEXTURE_SWIZZLE_RGBA on OpenGLES
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_R, swizzleMask[0]);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_G, swizzleMask[1]);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, swizzleMask[2]);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_A, swizzleMask[3]);
+#else
 			glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzleMask);
+#endif
 		}
+#ifdef _GLESxxx //karin: swap R and B as GL_BGRA on OpenGLES
+		else
+		{
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_R, GL_BLUE);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, GL_RED);
+		}
+#endif
 	}
 
 	if (deletebuffer && buffer) free(buffer);

Q3E/src/main/jni/gzdoom/src/common/rendering/gles/gles_shaderprogram.cpp
@@ -247,6 +247,17 @@ FString FShaderProgram::PatchShader(ShaderType type, const FString &code, const 
 
 
 	patchedCode << "#line 1\n";
+#ifdef _GLES //karin: convert GLSL code to 320 es on OpenGLES
+	if(gles.glesMode == GLES_MODE_OGL32)
+	{
+		FString newcode = RemoveLayoutLocationDecl(code, type == Vertex ? "out" : "in");
+		FString patch300 = GenGLSL300PatchCode((int)type);
+		patchedCode << patch300;
+		FString code300 = GLSL100_to_GLSL300(newcode, (int)type);
+		patchedCode << code300;
+	}
+	else
+#endif
 	patchedCode << RemoveLayoutLocationDecl(code, type == Vertex ? "out" : "in");
 
 	if (maxGlslVersion < 420)

Q3E/src/main/jni/gzdoom/src/common/rendering/hwrenderer/data/hw_shaderpatcher.h
@@ -29,3 +29,7 @@ struct FEffectShader
 extern const FDefaultShader defaultshaders[];
 extern const FEffectShader effectshaders[];
 
+#ifdef _GLES //karin: convert GLSL code to 300 es on OpenGLES
+FString GLSL100_to_GLSL300(FString code, int type);
+FString GenGLSL300PatchCode(int type);
+#endif

andreyvelsk/UZDoom

(cherry picked from commit 9c2093414ceb49ee8f069b0c0cf782c4ac15e8fd)

src/common/rendering/gl/gl_framebuffer.cpp
@@ -212,7 +212,22 @@ void OpenGLFrameBuffer::CopyScreenToBuffer(int width, int height, uint8_t* scr)
 
 	// strictly speaking not needed as the glReadPixels should block until the scene is rendered, but this is to safeguard against shitty drivers
 	glFinish();
+#ifdef ANDROID //karin: glReadPixels using GL_RGBA on OpenGLES
+	uint8_t *scr4 = (uint8_t *)malloc(4 * width * height);
+	glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, scr4);
+	for(int h = 0; h < height; h++)
+	{
+		for(int w = 0; w < width; w++)
+		{
+			const uint8_t *src = scr4 + (width * h + w) * 4;
+			uint8_t *dst = scr + (width * h + w) * 3;
+			memcpy(dst, src, 3);
+		}
+	}
+	free(scr4);
+#else
 	glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, scr);
+#endif
 }
 
 //===========================================================================
@@ -507,7 +522,22 @@ TArray<uint8_t> OpenGLFrameBuffer::GetScreenshotBuffer(int &pitch, ESSType &colo
 	TArray<uint8_t> pixels;
 	pixels.Resize(viewport.width * viewport.height * 3);
 	glPixelStorei(GL_PACK_ALIGNMENT, 1);
+#ifdef ANDROID //karin: glReadPixels using GL_RGBA on OpenGLES
+	uint8_t *pixels4 = (uint8_t *)malloc(4 * viewport.width * viewport.height);
+	glReadPixels(viewport.left, viewport.top, viewport.width, viewport.height, GL_RGBA, GL_UNSIGNED_BYTE, pixels4);
+	for(int h = 0; h < viewport.height; h++)
+	{
+		for(int w = 0; w < viewport.width; w++)
+		{
+			const uint8_t *src = pixels4 + (viewport.width * h + w) * 4;
+			uint8_t *dst = (&pixels[0]) + (viewport.width * h + w) * 3;
+			memcpy(dst, src, 3);
+		}
+	}
+	free(pixels4);
+#else
 	glReadPixels(viewport.left, viewport.top, viewport.width, viewport.height, GL_RGB, GL_UNSIGNED_BYTE, &pixels[0]);
+#endif
 	glPixelStorei(GL_PACK_ALIGNMENT, 4);
 
 	// Copy to screenshot buffer:

src/common/rendering/gl/gl_hwtexture.cpp
@@ -139,20 +139,35 @@ unsigned int FHardwareTexture::CreateTexture(unsigned char * buffer, int w, int 
 	{
 		if (glTextureBytes < 4) glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 		static const int ITypes[] = { GL_R8, GL_RG8, GL_RGB8, GL_RGBA8 };
+#ifdef ANDROID //karin: internal format must same as data format on OpenGLES
+		static const int STypes[] = { GL_RED, GL_RG, GL_RGB, GL_RGBA };
+#else
 		static const int STypes[] = { GL_RED, GL_RG, GL_BGR, GL_BGRA };
+#endif
 
 		texformat = ITypes[glTextureBytes - 1];
 		sourcetype = STypes[glTextureBytes - 1];
 	}
 	else
 	{
+#ifdef ANDROID //karin: internal format must same as data format on OpenGLES
+		sourcetype = GL_RGBA;
+#else
 		sourcetype = GL_BGRA;
+#endif
 	}
 
 	if (!firstCall && glBufferID > 0)
 		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, rw, rh, sourcetype, GL_UNSIGNED_BYTE, buffer);
 	else
 		glTexImage2D(GL_TEXTURE_2D, 0, texformat, rw, rh, 0, sourcetype, GL_UNSIGNED_BYTE, buffer);
+#ifdef ANDROID //karin: swap R and B on OpenGLES
+	if(sourcetype == GL_RGB || sourcetype == GL_RGBA)
+	{
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_R, GL_BLUE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, GL_RED);
+	}
+#endif
 
 	if (deletebuffer && buffer) free(buffer);
 	else if (glBufferID)

src/common/rendering/gl/gl_renderstate.cpp
@@ -424,7 +424,19 @@ void FGLRenderState::Draw(int dt, int index, int count, bool apply)
 		Apply();
 	}
 	drawcalls.Clock();
+#ifdef ANDROID //karin: TODO glDrawArrays crash!!!
+	int buf;
+	glGetIntegerv( GL_ELEMENT_ARRAY_BUFFER_BINDING, &buf);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	int *arr = (int*)malloc(4 * count);
+	for(int i = 0; i < count; i++)
+		arr[i] = index + i;
+	TID(glDrawElements(dt2gl[dt], count, GL_UNSIGNED_INT, arr));
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buf);
+	free(arr);
+#else
 	glDrawArrays(dt2gl[dt], index, count);
+#endif
 	drawcalls.Unclock();
 }
 

src/common/rendering/gl/gl_shaderprogram.cpp
@@ -32,6 +32,22 @@
 
 namespace OpenGLRenderer
 {
+#ifdef ANDROID //karin: control GLSL default precision
+CVAR(Int, gl_glsl_precision, 0, 0); // 0 = default(high), 1 = medium, 2 = low
+
+FString GetGLSLPrecision()
+{
+	FString str = "precision highp int;\n \
+			   precision highp float;\n";
+
+	if (gl_glsl_precision == 2)
+		str.Substitute("highp", "lowp");
+	else if (gl_glsl_precision == 1)
+		str.Substitute("highp", "mediump");
+
+	return str;
+}
+#endif
 
 bool IsShaderCacheActive();
 TArray<uint8_t> LoadCachedProgramBinary(const FString &vertex, const FString &fragment, uint32_t &binaryFormat);
@@ -266,7 +282,11 @@ FString FShaderProgram::PatchShader(ShaderType type, const FString &code, const 
 	// If we have 4.2, always use it because it adds important new syntax.
 	if (maxGlslVersion < 420 && gl.glslversion >= 4.2f) maxGlslVersion = 420;
 	int shaderVersion = min((int)round(gl.glslversion * 10) * 10, maxGlslVersion);
+#ifdef ANDROID //karin: using #version 320 es on OpenGLES
+	patchedCode.AppendFormat("#version 300 es\n");
+#else
 	patchedCode.AppendFormat("#version %d\n", shaderVersion);
+#endif
 
 	// TODO: Find some way to add extension requirements to the patching
 	//
@@ -277,8 +297,12 @@ FString FShaderProgram::PatchShader(ShaderType type, const FString &code, const 
 		patchedCode << defines;
 
 	// these settings are actually pointless but there seem to be some old ATI drivers that fail to compile the shader without setting the precision here.
+#ifdef ANDROID //karin: control GLSL default precision
+	patchedCode << GetGLSLPrecision();
+#else
 	patchedCode << "precision highp int;\n";
 	patchedCode << "precision highp float;\n";
+#endif
 
 	patchedCode << "#line 1\n";
 	patchedCode << RemoveLayoutLocationDecl(code, type == Vertex ? "out" : "in");

src/common/rendering/gles/gles_hwtexture.cpp
@@ -155,7 +155,11 @@ unsigned int FHardwareTexture::CreateTexture(unsigned char * buffer, int w, int 
 		else
 		{
 			sourcetype = GL_BGRA;
+#ifdef ANDROID //karin: These two must be the same on OpenGLES
+			texformat = GL_BGRA;
+#else
 			texformat = GL_RGBA;
+#endif
 		}
 	}
 
@@ -168,8 +172,22 @@ unsigned int FHardwareTexture::CreateTexture(unsigned char * buffer, int w, int 
 		if (glTextureBytes == 1)
 		{
 			GLint swizzleMask[] = { GL_ZERO, GL_ZERO, GL_ZERO, GL_RED };
+#ifdef ANDROID //karin: no GL_TEXTURE_SWIZZLE_RGBA on OpenGLES
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_R, swizzleMask[0]);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_G, swizzleMask[1]);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, swizzleMask[2]);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_A, swizzleMask[3]);
+#else
 			glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzleMask);
+#endif
 		}
+#ifdef _GLESxxx //karin: swap R and B as GL_BGRA on OpenGLES
+		else
+		{
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_R, GL_BLUE);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, GL_RED);
+		}
+#endif
 	}
 
 	if (deletebuffer && buffer) free(buffer);

src/common/rendering/gles/gles_shaderprogram.cpp
@@ -247,6 +247,17 @@ FString FShaderProgram::PatchShader(ShaderType type, const FString &code, const 
 
 
 	patchedCode << "#line 1\n";
+#ifdef ANDROID //karin: convert GLSL code to 320 es on OpenGLES
+	if(gles.glesMode == GLES_MODE_OGL32)
+	{
+		FString newcode = RemoveLayoutLocationDecl(code, type == Vertex ? "out" : "in");
+		FString patch300 = GenGLSL300PatchCode((int)type);
+		patchedCode << patch300;
+		FString code300 = GLSL100_to_GLSL300(newcode, (int)type);
+		patchedCode << code300;
+	}
+	else
+#endif
 	patchedCode << RemoveLayoutLocationDecl(code, type == Vertex ? "out" : "in");
 
 	if (maxGlslVersion < 420)

src/common/rendering/hwrenderer/data/hw_shaderpatcher.h
@@ -29,3 +29,7 @@ struct FEffectShader
 extern const FDefaultShader defaultshaders[];
 extern const FEffectShader effectshaders[];
 
+#ifdef ANDROID //karin: convert GLSL code to 300 es on OpenGLES
+FString GLSL100_to_GLSL300(FString code, int type);
+FString GenGLSL300PatchCode(int type);
+#endif